202ca73a933ebd86c5991e0e1492dd8580f8ef03
khan-guddu-02 <gareebnawazkhan78@gmail.com>
2025-11-25 22:48:39 +0530

    setup store slice and appwrite auth , cinfig

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
+import { useDispatch } from "react-redux";
 import "./App.css";
+import { useState, useEffect } from "react";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Outlet } from "react-router-dom";
+import { Footer, Header } from './components'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-      <h3>Mega Blog..</h3>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+  authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({ userData }));
+      } else {
+        dispatch(logout()); // guest users ke liye
+      }
+    })
+    .finally(() => setLoading(false));
+}, []);
+
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content=between bg-gray-400 ">
+      <div className="w-full block">
+        <Header />
+        <main>{/* <Outlet/> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
